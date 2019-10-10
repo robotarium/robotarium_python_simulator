3663bb1d64d11a8959dbad23887a78a07b9e5245
Sean Wilson <swilson64@gatech.edu>
2019-10-10 15:06:06 -0400

    Updates to controllers and transformations.

--- rps/utilities/controllers.py
+++ rps/utilities/controllers.py
@@ -1,28 +1,61 @@
 import numpy as np
 from rps.utilities.transformations import *
 
-def single_integrator_position_controller(x, poses, gain=1, magnitude_limit=0.08):
-    """A position controller for single integrators.  Drives a single integrator to a point
+def create_si_position_controller(x_velocity_gain=1, y_velocity_gain=1, velocity_magnitude_limit=0.15):
+    """Creates a position controller for single integrators.  Drives a single integrator to a point
     using a propoertional controller.
 
-    x: 2xN numpy array (of single-integrator states)
-    poses: 2xN numpy array (of desired poses)
-    gain: double (proportional gain for controller)
-    magnitude_limit: double (limit of the magnitude of the single-integrator control input
-    produced)
+    x_velocity_gain - the gain impacting the x (horizontal) velocity of the single integrator
+    y_velocity_gain - the gain impacting the y (vertical) velocity of the single integrator
+    velocity_magnitude_limit - the maximum magnitude of the produce velocity vector (should be less than the max linear speed of the platform)
 
-    -> 2xN numpy array (of single-integrator control inputs)
+    -> function
     """
 
-    # Calculate control input
-    dxi = gain*(poses-x)
+    #Check user input types
+    assert isinstance(x_velocity_gain, (int, float)), "In the function create_si_position_controller, the x linear velocity gain (x_velocity_gain) must be an integer or float. Recieved type %r." % type(x_velocity_gain).__name__
+    assert isinstance(y_velocity_gain, (int, float)), "In the function create_si_position_controller, the y linear velocity gain (y_velocity_gain) must be an integer or float. Recieved type %r." % type(y_velocity_gain).__name__
+    assert isinstance(velocity_magnitude_limit, (int, float)), "In the function create_si_position_controller, the velocity magnitude limit (y_velocity_gain) must be an integer or float. Recieved type %r." % type(y_velocity_gain).__name__
+    
+    #Check user input ranges/sizes
+    assert x_velocity_gain > 0, "In the function create_si_position_controller, the x linear velocity gain (x_velocity_gain) must be positive. Recieved %r." % x_velocity_gain
+    assert y_velocity_gain > 0, "In the function create_si_position_controller, the y linear velocity gain (y_velocity_gain) must be positive. Recieved %r." % y_velocity_gain
+    assert velocity_magnitude_limit >= 0, "In the function create_si_position_controller, the velocity magnitude limit (velocity_magnitude_limit) must not be negative. Recieved %r." % velocity_magnitude_limit
+    
+    gain = np.diag([x_velocity_gain, y_velocity_gain])
 
-    # Threshold magnitude
-    norms = np.linalg.norm(dxi, axis=0)
-    idxs = np.where(norms > magnitude_limit)
-    dxi[:, idxs] *= magnitude_limit/norms[idxs]
+    def si_position_controller(xi, positions):
 
-    return dxi
+        """
+        x: 2xN numpy array (of single-integrator states of the robots)
+        points: 2xN numpy array (of desired points each robot should achieve)
+
+        -> 2xN numpy array (of single-integrator control inputs)
+
+        """
+
+        #Check user input types
+        assert isinstance(xi, np.ndarray), "In the si_position_controller function created by the create_si_position_controller function, the single-integrator robot states (xi) must be a numpy array. Recieved type %r." % type(xi).__name__
+        assert isinstance(positions, np.ndarray), "In the si_position_controller function created by the create_si_position_controller function, the robot goal points (positions) must be a numpy array. Recieved type %r." % type(positions).__name__
+
+        #Check user input ranges/sizes
+        assert xi.shape[0] == 2, "In the si_position_controller function created by the create_si_position_controller function, the dimension of the single-integrator robot states (xi) must be 2 ([x;y]). Recieved dimension %r." % xi.shape[0]
+        assert positions.shape[0] == 2, "In the si_position_controller function created by the create_si_position_controller function, the dimension of the robot goal points (positions) must be 2 ([x_goal;y_goal]). Recieved dimension %r." % positions.shape[0]
+        assert xi.shape[1] == positions.shape[1], "In the si_position_controller function created by the create_si_position_controller function, the number of single-integrator robot states (xi) must be equal to the number of robot goal points (positions). Recieved a single integrator velocity input array of size %r x %r and current pose array of size %r x %r." % (xi.shape[0], xi.shape[1], positions.shape[0], positions.shape[1])
+
+
+        # Calculate control input
+        dxi = gain*(positions-xi)
+
+        # Threshold magnitude
+        norms = np.linalg.norm(dxi, axis=0)
+        idxs = np.where(norms > magnitude_limit)
+        if idxs.size != 0:
+            dxi[:, idxs] *= magnitude_limit/norms[idxs]
+
+        return dxi
+
+    return si_position_controller
 
 def unicycle_pose_controller(x, poses, magnitude_limit=0.25, position_error=0.01, rotation_error=0.25):
     """  A pose controller for unicycle models.  This is a hybrid controller that first
--- rps/utilities/transformations.py
+++ rps/utilities/transformations.py
@@ -1,37 +1,123 @@
 import numpy as np
 
-def single_integrator_to_unicycle2(dxi, poses, linear_velocity_gain=1, angular_velocity_limit=4*np.pi):
-    """A mapping from single-integrator to unicycle dynamics.
+def create_si_to_uni_dynamics(linear_velocity_gain=1, angular_velocity_limit=np.pi):
+    """ Returns a function mapping from single-integrator to unicycle dynamics with angular velocity magnitude restrictions.
 
-    dxi: 2xN numpy array with single-integrator control inputs
-    poses: 2xN numpy array with single-integrator poses
-    linear_velocity_gain: Gain for unicycle linear velocity
-    angular_velocity_limit: Limit for angular velocity (i.e., |w| < angular_velocity_limit)
+        linear_velocity_gain: Gain for unicycle linear velocity
+        angular_velocity_limit: Limit for angular velocity (i.e., |w| < angular_velocity_limit)
 
-    -> 2xN numpy array of unicycle control inputs
+        -> function
     """
 
-    M,N = np.shape(dxi)
+    #Check user input types
+    assert isinstance(linear_velocity_gain, (int, float)), "In the function create_si_to_uni_dynamics, the linear velocity gain (linear_velocity_gain) must be an integer or float. Recieved type %r." % type(linear_velocity_gain).__name__
+    assert isinstance(angular_velocity_limit, (int, float)), "In the function create_si_to_uni_dynamics, the angular velocity limit (angular_velocity_limit) must be an integer or float. Recieved type %r." % type(angular_velocity_limit).__name__
 
-    a = np.cos(poses[2, :])
-    b = np.sin(poses[2, :])
+    #Check user input ranges/sizes
+    assert linear_velocity_gain > 0, "In the function create_si_to_uni_dynamics, the linear velocity gain (linear_velocity_gain) must be positive. Recieved %r." % linear_velocity_gain
+    assert angular_velocity_limit >= 0, "In the function create_si_to_uni_dynamics, the angular velocity limit (angular_velocity_limit) must not be negative. Recieved %r." % angular_velocity_limit
+    
 
-    dxu = np.zeros((2, N))
-    dxu[0, :] = linear_velocity_gain*(a*dxi[0, :] + b*dxi[1, :])
-    dxu[1, :] = (np.pi/2)*angular_velocity_limit*np.arctan2(-b*dxi[0, :] + a*dxi[1, :], dxu[0, :])
+    def si_to_uni_dyn(dxi, poses):
+        """A mapping from single-integrator to unicycle dynamics.
 
-    return dxu
+        dxi: 2xN numpy array with single-integrator control inputs
+        poses: 2xN numpy array with single-integrator poses
 
-def create_single_integrator_to_unicycle(projection_distance=0.05):
-    """Creates a mapping from single integrator to unicycle dynamics by placing
-    a virtual "point" in front of the unicycle.
+        -> 2xN numpy array of unicycle control inputs
+        """
+
+        #Check user input types
+        assert isinstance(dxi, np.ndarray), "In the si_to_uni_dyn function created by the create_si_to_uni_dynamics function, the single integrator velocity inputs (dxi) must be a numpy array. Recieved type %r." % type(dxi).__name__
+        assert isinstance(poses, np.ndarray), "In the si_to_uni_dyn function created by the create_si_to_uni_dynamics function, the current robot poses (poses) must be a numpy array. Recieved type %r." % type(poses).__name__
+
+        #Check user input ranges/sizes
+        assert dxi.shape[0] == 2, "In the si_to_uni_dyn function created by the create_si_to_uni_dynamics function, the dimension of the single integrator velocity inputs (dxi) must be 2 ([x_dot;y_dot]). Recieved dimension %r." % dxi.shape[0]
+        assert poses.shape[0] == 3, "In the si_to_uni_dyn function created by the create_si_to_uni_dynamics function, the dimension of the current pose of each robot must be 3 ([x;y;theta]). Recieved dimension %r." % poses.shape[0]
+        assert states.shape[1] == poses.shape[1], "In the si_to_uni_dyn function created by the create_si_to_uni_dynamics function, the number of single integrator velocity inputs must be equal to the number of current robot poses. Recieved a single integrator velocity input array of size %r x %r and current pose array of size %r x %r." % (dxi.shape[0], dxi.shape[1], poses.shape[0], poses.shape[1])
+
+        M,N = np.shape(dxi)
+
+        a = np.cos(poses[2, :])
+        b = np.sin(poses[2, :])
+
+        dxu = np.zeros((2, N))
+        dxu[0, :] = linear_velocity_gain*(a*dxi[0, :] + b*dxi[1, :])
+        dxu[1, :] = angular_velocity_limit*np.arctan2(-b*dxi[0, :] + a*dxi[1, :], dxu[0, :])/(np.pi/2)
+
+        return dxu
+
+    return si_to_uni_dyn
+
+def create_si_to_uni_dynamics_with_backwards_motion(linear_velocity_gain=1, angular_velocity_limit=np.pi):
+    """ Returns a function mapping from single-integrator dynamics to unicycle dynamics. This implementation of 
+    the mapping allows for robots to drive backwards if that direction of linear velocity requires less rotation.
+
+        linear_velocity_gain: Gain for unicycle linear velocity
+        angular_velocity_limit: Limit for angular velocity (i.e., |w| < angular_velocity_limit)
+
+    """
+
+    #Check user input types
+    assert isinstance(linear_velocity_gain, (int, float)), "In the function create_si_to_uni_dynamics, the linear velocity gain (linear_velocity_gain) must be an integer or float. Recieved type %r." % type(linear_velocity_gain).__name__
+    assert isinstance(angular_velocity_limit, (int, float)), "In the function create_si_to_uni_dynamics, the angular velocity limit (angular_velocity_limit) must be an integer or float. Recieved type %r." % type(angular_velocity_limit).__name__
+
+    #Check user input ranges/sizes
+    assert linear_velocity_gain > 0, "In the function create_si_to_uni_dynamics, the linear velocity gain (linear_velocity_gain) must be positive. Recieved %r." % linear_velocity_gain
+    assert angular_velocity_limit >= 0, "In the function create_si_to_uni_dynamics, the angular velocity limit (angular_velocity_limit) must not be negative. Recieved %r." % angular_velocity_limit
+    
+
+    def si_to_uni_dyn(dxi, poses):
+        """A mapping from single-integrator to unicycle dynamics.
+
+        dxi: 2xN numpy array with single-integrator control inputs
+        poses: 2xN numpy array with single-integrator poses
+
+        -> 2xN numpy array of unicycle control inputs
+        """
+
+        #Check user input types
+        assert isinstance(dxi, np.ndarray), "In the si_to_uni_dyn function created by the create_si_to_uni_dynamics_with_backwards_motion function, the single integrator velocity inputs (dxi) must be a numpy array. Recieved type %r." % type(dxi).__name__
+        assert isinstance(poses, np.ndarray), "In the si_to_uni_dyn function created by the create_si_to_uni_dynamics_with_backwards_motion function, the current robot poses (poses) must be a numpy array. Recieved type %r." % type(poses).__name__
+
+        #Check user input ranges/sizes
+        assert dxi.shape[0] == 2, "In the si_to_uni_dyn function created by the create_si_to_uni_dynamics_with_backwards_motion function, the dimension of the single integrator velocity inputs (dxi) must be 2 ([x_dot;y_dot]). Recieved dimension %r." % dxi.shape[0]
+        assert poses.shape[0] == 3, "In the si_to_uni_dyn function created by the create_si_to_uni_dynamics_with_backwards_motion function, the dimension of the current pose of each robot must be 3 ([x;y;theta]). Recieved dimension %r." % poses.shape[0]
+        assert states.shape[1] == poses.shape[1], "In the si_to_uni_dyn function created by the create_si_to_uni_dynamics_with_backwards_motion function, the number of single integrator velocity inputs must be equal to the number of current robot poses. Recieved a single integrator velocity input array of size %r x %r and current pose array of size %r x %r." % (dxi.shape[0], dxi.shape[1], poses.shape[0], poses.shape[1])
+
+        M,N = np.shape(dxi)
+
+        a = np.cos(poses[2, :])
+        b = np.sin(poses[2, :])
+
+        dxu = np.zeros((2, N))
+        dxu[0, :] = linear_velocity_gain*(a*dxi[0, :] + b*dxi[1, :])
+        dxu[1, :] = angular_velocity_limit*np.arctan2(-b*dxi[0, :] + a*dxi[1, :], dxu[0, :])/(np.pi/2)
+
+        return dxu
+
+    return si_to_uni_dyn
+
+def create_si_to_uni_mapping(projection_distance=0.05):
+    """Creates two functions for mapping from single integrator dynamics to 
+    unicycle dynamics and unicycle states to single integrator states. 
+    
+    This mapping is done by placing a virtual control "point" in front of 
+    the unicycle.
 
     projection_distance: How far ahead to place the point
 
     -> (function, function)
     """
 
-    def f1(dxi, poses):
+    #Check user input types
+    assert isinstance(projection_distance, (int, float)), "In the function create_si_to_uni_mapping, the projection distance of the new control point (projection_distance) must be an integer or float. Recieved type %r." % type(projection_distance).__name__
+    
+    #Check user input ranges/sizes
+    assert projection_distance > 0, "In the function create_si_to_uni_mapping, the projection distance of the new control point (projection_distance) must be positive. Recieved %r." % projection_distance
+    
+
+    def si_to_uni_dyn(dxi, poses):
         """Takes single-integrator velocities and transforms them to unicycle
         control inputs.
 
@@ -40,6 +126,16 @@ def create_single_integrator_to_unicycle(projection_distance=0.05):
 
         -> 2xN numpy array of unicycle control inputs
         """
+
+        #Check user input types
+        assert isinstance(dxi, np.ndarray), "In the si_to_uni_dyn function created by the create_si_to_uni_mapping function, the single integrator velocity inputs (dxi) must be a numpy array. Recieved type %r." % type(dxi).__name__
+        assert isinstance(poses, np.ndarray), "In the si_to_uni_dyn function created by the create_si_to_uni_mapping function, the current robot poses (poses) must be a numpy array. Recieved type %r." % type(poses).__name__
+
+        #Check user input ranges/sizes
+        assert dxi.shape[0] == 2, "In the si_to_uni_dyn function created by the create_si_to_uni_mapping function, the dimension of the single integrator velocity inputs (dxi) must be 2 ([x_dot;y_dot]). Recieved dimension %r." % dxi.shape[0]
+        assert poses.shape[0] == 3, "In the si_to_uni_dyn function created by the create_si_to_uni_mapping function, the dimension of the current pose of each robot must be 3 ([x;y;theta]). Recieved dimension %r." % poses.shape[0]
+        assert states.shape[1] == poses.shape[1], "In the si_to_uni_dyn function created by the create_si_to_uni_mapping function, the number of single integrator velocity inputs must be equal to the number of current robot poses. Recieved a single integrator velocity input array of size %r x %r and current pose array of size %r x %r." % (dxi.shape[0], dxi.shape[1], poses.shape[0], poses.shape[1])
+
 
         M,N = np.shape(dxi)
 
@@ -52,7 +148,7 @@ def create_single_integrator_to_unicycle(projection_distance=0.05):
 
         return dxu
 
-    def f2(poses):
+    def uni_to_si_states(poses):
         """Takes unicycle states and returns single-integrator states
 
         poses: 3xN numpy array of unicycle states
@@ -68,26 +164,57 @@ def create_single_integrator_to_unicycle(projection_distance=0.05):
 
         return si_states
 
-    return f1, f2
+    return si_to_uni_dyn, uni_to_si_states
 
-def unicycle_to_single_integrator(dxu, poses, projection_distance=0.05):
-    """A function for converting from unicycle to single-integrator dynamics.
-    Utilizes a virtual point placed in front of the unicycle.
+def create_uni_to_si_dynamics(projection_distance=0.05):
+    """Creates two functions for mapping from unicycle dynamics to single 
+    integrator dynamics and single integrator states to unicycle states. 
+    
+    This mapping is done by placing a virtual control "point" in front of 
+    the unicycle.
 
-    dxu: 2xN numpy array of unicycle control inputs
-    poses: 3xN numpy array of unicycle poses
-    projection_distance: How far ahead of the unicycle model to place the point
+    projection_distance: How far ahead to place the point
 
-    -> 2xN numpy array of single-integrator control inputs
+    -> function
     """
 
-    M,N = np.shape(dxu)
+    #Check user input types
+    assert isinstance(projection_distance, (int, float)), "In the function create_uni_to_si_dynamics, the projection distance of the new control point (projection_distance) must be an integer or float. Recieved type %r." % type(projection_distance).__name__
+    
+    #Check user input ranges/sizes
+    assert projection_distance > 0, "In the function create_uni_to_si_dynamics, the projection distance of the new control point (projection_distance) must be positive. Recieved %r." % projection_distance
+    
 
-    cs = np.cos(poses[2, :])
-    ss = np.sin(poses[2, :])
+    def uni_to_si_dyn(dxu, poses):
+        """A function for converting from unicycle to single-integrator dynamics.
+        Utilizes a virtual point placed in front of the unicycle.
 
-    dxi = np.zeros((2, N))
-    dxi[0, :] = (cs*dxu[0, :] - projection_distance*ss*dxu[1, :])
-    dxi[1, :] = (ss*dxu[0, :] + projection_distance*cs*dxu[1, :])
+        dxu: 2xN numpy array of unicycle control inputs
+        poses: 3xN numpy array of unicycle poses
+        projection_distance: How far ahead of the unicycle model to place the point
 
-    return dxi
+        -> 2xN numpy array of single-integrator control inputs
+        """
+
+        #Check user input types
+        assert isinstance(dxu, np.ndarray), "In the uni_to_si_dyn function created by the create_uni_to_si_dynamics function, the unicycle velocity inputs (dxu) must be a numpy array. Recieved type %r." % type(dxi).__name__
+        assert isinstance(poses, np.ndarray), "In the uni_to_si_dyn function created by the create_uni_to_si_dynamics function, the current robot poses (poses) must be a numpy array. Recieved type %r." % type(poses).__name__
+
+        #Check user input ranges/sizes
+        assert dxu.shape[0] == 2, "In the uni_to_si_dyn function created by the create_uni_to_si_dynamics function, the dimension of the unicycle velocity inputs (dxu) must be 2 ([v;w]). Recieved dimension %r." % dxu.shape[0]
+        assert poses.shape[0] == 3, "In the uni_to_si_dyn function created by the create_uni_to_si_dynamics function, the dimension of the current pose of each robot must be 3 ([x;y;theta]). Recieved dimension %r." % poses.shape[0]
+        assert states.shape[1] == poses.shape[1], "In the uni_to_si_dyn function created by the create_uni_to_si_dynamics function, the number of unicycle velocity inputs must be equal to the number of current robot poses. Recieved a unicycle velocity input array of size %r x %r and current pose array of size %r x %r." % (dxu.shape[0], dxu.shape[1], poses.shape[0], poses.shape[1])
+
+        
+        M,N = np.shape(dxu)
+
+        cs = np.cos(poses[2, :])
+        ss = np.sin(poses[2, :])
+
+        dxi = np.zeros((2, N))
+        dxi[0, :] = (cs*dxu[0, :] - projection_distance*ss*dxu[1, :])
+        dxi[1, :] = (ss*dxu[0, :] + projection_distance*cs*dxu[1, :])
+
+        return dxi
+
+    return uni_to_si_dyn
