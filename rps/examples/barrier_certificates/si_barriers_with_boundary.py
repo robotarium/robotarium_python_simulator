720b3636ee9519e977b66c954b43131552a4712c
M-Usamah <mohammad.usamah127@hotmail.com>
2024-10-24 18:19:53 +0500

    update numpy

--- rps/examples/barrier_certificates/si_barriers_with_boundary.py
+++ rps/examples/barrier_certificates/si_barriers_with_boundary.py
@@ -8,14 +8,14 @@ import numpy as np
 
 # Instantiate Robotarium object
 N = 5
-initial_conditions = np.array(np.mat('1 0.5 -0.5 0 0.28; 0.8 -0.3 -0.75 0.1 0.34; 0 0 0 0 0'))
+initial_conditions = np.array(np.asmatrix('1 0.5 -0.5 0 0.28; 0.8 -0.3 -0.75 0.1 0.34; 0 0 0 0 0'))
 r = robotarium.Robotarium(number_of_robots=N, show_figure=True, initial_conditions=initial_conditions,sim_in_real_time=True)
 
 # The robots will never reach their goal points so set iteration number
 iterations = 3000
 
 # Define goal points outside of the arena
-goal_points = np.array(np.mat('5 -5 5 -5 5; 5 5 -5 -5 5; 0 0 0 0 0'))
+goal_points = np.array(np.asmatrix('5 -5 5 -5 5; 5 5 -5 -5 5; 0 0 0 0 0'))
 
 # Create unicycle position controller
 si_position_controller = create_si_position_controller()
